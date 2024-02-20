e96809d53dce3e3446b6e225781c4282b13022f6
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-20 19:33:15 -0300

    feat: added base DTOs for later use

---
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/dto/UserResponseDto.java
@@ -0,0 +1,23 @@
+package com.compassuol.sp.challenge.msuser.web.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.Date;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class UserResponseDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String cpf;
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    private Date birthdate;
+    private String email;
+    private String cep;
+    private String password;
+    private Boolean active;
+}
