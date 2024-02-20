a2680b24880acaa70b1b065f3973a32bdbaf3a07
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-20 20:08:57 -0300

    feat: added mappers to deal with DTOs

---
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/dto/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.compassuol.sp.challenge.msuser.web.dto.mapper;
+
+import com.compassuol.sp.challenge.msuser.domain.model.User;
+import com.compassuol.sp.challenge.msuser.web.dto.UserCreateDto;
+import com.compassuol.sp.challenge.msuser.web.dto.UserResponseDto;
+import org.modelmapper.ModelMapper;
+
+public class UserMapper {
+
+        public static UserResponseDto toDto(User user){
+            return new ModelMapper().map(user, UserResponseDto.class);
+        }
+
+        public static User toUser(UserCreateDto dto){
+            return new ModelMapper().map(dto, User.class);
+        }
+
+}
