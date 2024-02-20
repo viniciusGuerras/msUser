b15582485e0f6c1404fbd82896b041886078bcb4
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-20 16:07:05 -0300

    feat: added user entity

--- src/main/java/com/compassuol/sp/challenge/msuser/domain/model/User.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/domain/model/User.java
@@ -1,4 +1,57 @@
 package com.compassuol.sp.challenge.msuser.domain.model;
 
-public class User {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Entity
+@Table(schema = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", length = 120, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 120, nullable = false)
+    private String lastName;
+
+    @Column(name = "cpf", unique = true, length = 11, nullable = false)
+    private String cpf;
+
+    @Column(name = "birthdate", nullable = false)
+    private Date birthdate;
+
+    @Column(name = "email", length = 120, nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "cep", nullable = false)
+    private String cep;
+
+    @Column(name = "password", length = 120, nullable = false)
+    private String password;
+
+    @Column(name = "active", nullable = false)
+    private boolean active;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
