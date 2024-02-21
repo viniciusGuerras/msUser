88da93a26ca8fd788ae9d4f8dfa594fffee8ed16
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-20 22:03:46 -0300

    feat: created findById endpoint

--- src/main/java/com/compassuol/sp/challenge/msuser/domain/service/UserService.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/domain/service/UserService.java
@@ -2,6 +2,7 @@ package com.compassuol.sp.challenge.msuser.domain.service;
 
 import com.compassuol.sp.challenge.msuser.domain.model.User;
 import com.compassuol.sp.challenge.msuser.domain.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +16,12 @@ public class UserService {
     @Transactional
     public User create(User user){
         return userRepository.save(user);
+    }
+
+    @Transactional
+    public User findById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Usuário com o ID " + id + " não encontrado"));
     }
 
 }
