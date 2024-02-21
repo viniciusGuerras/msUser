3cf7f27a1a652c4c2ac8597d5a2e1e5d8d4a0d95
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-20 22:28:37 -0300

    feat: created DTO for necessary update data

---
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/dto/UserUpdateDto.java
@@ -0,0 +1,43 @@
+package com.compassuol.sp.challenge.msuser.web.dto;
+
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class UserUpdateDto {
+
+    @NotBlank
+    @Size(min = 3)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 3)
+    private String lastName;
+
+    @NotBlank
+    @CPF
+    private String cpf;
+
+    @NotNull
+    @Past
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birthdate;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Pattern(regexp = "^\\d{5}-\\d{3}$")
+    private String cep;
+
+    @NotNull
+    private Boolean active;
+}
