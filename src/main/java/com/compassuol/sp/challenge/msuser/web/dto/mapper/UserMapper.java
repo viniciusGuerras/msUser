582755ad998c2e44cc8aa139f326049babc525a2
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-22 11:48:28 -0300

    refactor: changed Mapper's toUser method to be Generic

--- src/main/java/com/compassuol/sp/challenge/msuser/web/dto/mapper/UserMapper.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/dto/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.compassuol.sp.challenge.msuser.web.dto.mapper;
 
 import com.compassuol.sp.challenge.msuser.domain.model.User;
 import com.compassuol.sp.challenge.msuser.web.dto.UserCreateDto;
+import com.compassuol.sp.challenge.msuser.web.dto.UserPasswordDto;
 import com.compassuol.sp.challenge.msuser.web.dto.UserResponseDto;
 import org.modelmapper.ModelMapper;
 
@@ -11,8 +12,7 @@ public class UserMapper {
             return new ModelMapper().map(user, UserResponseDto.class);
         }
 
-        public static User toUser(UserCreateDto dto){
+        public static <T> User toUser(T dto){
             return new ModelMapper().map(dto, User.class);
         }
-
 }
