44a9bbb7a647e56f39b6a041e38b6d83ab7ee7cd
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-26 11:44:45 -0300

    fix: removed unused methods

--- src/main/java/com/compassuol/sp/challenge/msuser/domain/rabbitMq/RabbitProducer.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/domain/rabbitMq/RabbitProducer.java
@@ -4,7 +4,6 @@ import com.compassuol.sp.challenge.msuser.domain.exceptions.MessageConversionExc
 import com.compassuol.sp.challenge.msuser.domain.model.Notification;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -31,7 +30,6 @@ public class RabbitProducer {
         public void sendMessage(Notification dto){
             try {
                 ObjectMapper objectMapper = new ObjectMapper();
-                objectMapper.registerModule(new JavaTimeModule());
                 byte[] payload = objectMapper.writeValueAsBytes(dto);
                 LOGGER.info(String.format("Message sent -> %s", dto.getEvent()));
                 rabbitTemplate.convertAndSend(exchange, routingKey, payload);
