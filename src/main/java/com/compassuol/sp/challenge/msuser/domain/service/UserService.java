701b9216deb301caf1ab7078ad3938101f1dd8da
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-26 09:26:34 -0300

    feat: added rabbitmq to methods

--- src/main/java/com/compassuol/sp/challenge/msuser/domain/service/UserService.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/domain/service/UserService.java
@@ -4,12 +4,16 @@ import com.compassuol.sp.challenge.msuser.domain.exceptions.EntityNotFoundExcept
 import com.compassuol.sp.challenge.msuser.domain.exceptions.UniqueFieldValidationException;
 import com.compassuol.sp.challenge.msuser.domain.model.User;
 import com.compassuol.sp.challenge.msuser.domain.repository.UserRepository;
-import com.compassuol.sp.challenge.msuser.openFeing.MsAddressConsumer;
+import com.compassuol.sp.challenge.msuser.domain.openFeing.MsAddressConsumer;
+import com.compassuol.sp.challenge.msuser.domain.rabbitMq.RabbitProducer;
+import com.compassuol.sp.challenge.msuser.domain.model.Notification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.compassuol.sp.challenge.msuser.domain.enums.EventTypeEnumeration.*;
 
 @Service
 @RequiredArgsConstructor
@@ -21,11 +25,15 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    private final RabbitProducer rabbitProducer;
+
     @Transactional
     public User create(User user){
+
         try {
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             msAddressConsumer.saveAddress(user.getCep());
+            rabbitProducer.sendMessage(new Notification(user.getEmail(), CREATE));
             return userRepository.save(user);
         }
         catch (DataIntegrityViolationException ex)
@@ -36,12 +44,13 @@ public class UserService {
 
     @Transactional(readOnly = true)
     public User findById(Long id) {
+
         return userRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("User with ID:" + id + " not found"));
     }
 
     @Transactional
-    public void updateInfo(Long id, User newUser) {
+    public void updateInfo(Long id, User newUser){
         User oldUser = findById(id);
         oldUser.setFirstName(newUser.getFirstName());
         oldUser.setLastName(newUser.getLastName());
@@ -50,11 +59,13 @@ public class UserService {
         oldUser.setEmail(newUser.getEmail());
         oldUser.setCep(newUser.getCep());
         oldUser.setActive(newUser.getActive());
+        rabbitProducer.sendMessage(new Notification(newUser.getEmail(), UPDATE));
     }
 
     @Transactional
     public void updatePassword(Long id, User user) {
         User foundUser = findById(id);
         foundUser.setPassword(passwordEncoder.encode(user.getPassword()));
+        rabbitProducer.sendMessage(new Notification(foundUser.getEmail(), UPDATE_PASSWORD));
     }
 }
