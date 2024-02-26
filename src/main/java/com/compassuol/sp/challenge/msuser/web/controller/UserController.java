6e50062471cc2ba2e38c8db13d27a0ed006dbf6b
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-26 09:33:56 -0300

    style: removed unused imports

--- src/main/java/com/compassuol/sp/challenge/msuser/web/controller/UserController.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/controller/UserController.java
@@ -19,8 +19,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.http.HttpResponse;
-
 
 @RestController
 @RequiredArgsConstructor
