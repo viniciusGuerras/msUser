010b399a4ae0c2b028e6d502cf8a2bc14e571805
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-28 13:34:44 -0300

    refactor: changed Long to DTO

--- src/main/java/com/compassuol/sp/challenge/msuser/domain/openFeing/MsAddressConsumer.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/domain/openFeing/MsAddressConsumer.java
@@ -1,16 +1,18 @@
 package com.compassuol.sp.challenge.msuser.domain.openFeing;
 
+import com.compassuol.sp.challenge.msuser.web.dto.AddressResponseDto;
 import com.compassuol.sp.challenge.msuser.web.dto.CepDto;
 import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 
 @FeignClient(value = "addressConsumer", url = "localhost:8081/v1/address/")
 public interface MsAddressConsumer {
 
     @PostMapping
-    Long saveAddress(@Valid @RequestBody CepDto dto);
+    AddressResponseDto saveAddress(@Valid @RequestBody CepDto dto, @RequestHeader("Authorization") String token);
 
 }
