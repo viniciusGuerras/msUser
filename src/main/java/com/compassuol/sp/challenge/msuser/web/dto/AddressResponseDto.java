822721a68a444713f727f916802fac390e3d93e5
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-28 13:35:50 -0300

    feat: created id DTO

---
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/dto/AddressResponseDto.java
@@ -0,0 +1,14 @@
+package com.compassuol.sp.challenge.msuser.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressResponseDto {
+    private Long id;
+}
