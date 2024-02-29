3a5265b3cad09d95bfc4d00b6a77d47d22def6c5
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-28 22:43:44 -0300

    feat: handled new exception

--- src/main/java/com/compassuol/sp/challenge/msuser/web/exception/ApiExceptionHandler.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/exception/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.compassuol.sp.challenge.msuser.web.exception;
 
+import com.compassuol.sp.challenge.msuser.domain.exceptions.CepNotFoundException;
 import com.compassuol.sp.challenge.msuser.domain.exceptions.EntityNotFoundException;
 import com.compassuol.sp.challenge.msuser.domain.exceptions.UniqueFieldValidationException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,13 +16,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, RuntimeException.class})
     public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request, BindingResult result){
         log.error("Api Error - ", ex);
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY,"Invalid field(s)", result));
     }
+
 
     @ExceptionHandler(UniqueFieldValidationException.class)
     public ResponseEntity<ErrorMessage> uniqueViolationException(RuntimeException ex,HttpServletRequest request){
@@ -31,7 +34,7 @@ public class ApiExceptionHandler {
                 .body(new ErrorMessage(request, HttpStatus.CONFLICT,ex.getMessage()));
     }
 
-    @ExceptionHandler(EntityNotFoundException.class)
+    @ExceptionHandler({EntityNotFoundException.class, CepNotFoundException.class})
     public ResponseEntity<ErrorMessage> entityNotFoundException(RuntimeException ex,HttpServletRequest request){
         log.error("Api Error - ", ex);
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
