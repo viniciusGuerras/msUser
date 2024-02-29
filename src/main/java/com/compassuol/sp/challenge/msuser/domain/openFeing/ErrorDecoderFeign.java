04aeae518c1087f0345a600ceca7e102e8792b91
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-02-28 22:52:39 -0300

    style: changed to enhanced switch

--- src/main/java/com/compassuol/sp/challenge/msuser/domain/openFeing/ErrorDecoderFeign.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/domain/openFeing/ErrorDecoderFeign.java
@@ -8,15 +8,10 @@ public class ErrorDecoderFeign implements ErrorDecoder {
 
     @Override
     public Exception decode(String methodKey, Response response) {
-        switch (response.status()) {
-            case 404: {
-                return new CepNotFoundException("Cep not found");
-            }
-            case 422:{
-                return new RuntimeException("Cep not valid");
-            }
-            default:
-                return new Exception(response.reason());
-        }
+        return switch (response.status()) {
+            case 404 -> new CepNotFoundException("Cep not found");
+            case 422 -> new RuntimeException("Cep not valid");
+            default -> new Exception(response.reason());
+        };
     }
 }
