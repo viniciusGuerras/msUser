f1681bf8cd2fe2f8d347b96983ed8c676fd23727
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-03-01 02:01:55 -0300

    feat: added max size for variables in login request

--- src/main/java/com/compassuol/sp/challenge/msuser/web/dto/LoginRequestDto.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/dto/LoginRequestDto.java
@@ -3,18 +3,21 @@ package com.compassuol.sp.challenge.msuser.web.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class LoginRequestDto {
 
     @NotBlank
     @Email
+    @Size(max = 120)
     private String email;
 
     @NotBlank
-    @Size(min = 6)
+    @Size(max = 160)
     private String password;
 }
