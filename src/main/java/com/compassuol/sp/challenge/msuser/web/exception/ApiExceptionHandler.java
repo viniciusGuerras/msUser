381923e135dcddda9738f3fb9f8acc87ef452471
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-03-01 02:04:32 -0300

    feat: added new exceptions to handler

--- src/main/java/com/compassuol/sp/challenge/msuser/web/exception/ApiExceptionHandler.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/exception/ApiExceptionHandler.java
@@ -2,23 +2,37 @@ package com.compassuol.sp.challenge.msuser.web.exception;
 
 import com.compassuol.sp.challenge.msuser.domain.exceptions.CepNotFoundException;
 import com.compassuol.sp.challenge.msuser.domain.exceptions.EntityNotFoundException;
+import com.compassuol.sp.challenge.msuser.domain.exceptions.TokenInvalidException;
 import com.compassuol.sp.challenge.msuser.domain.exceptions.UniqueFieldValidationException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, RuntimeException.class})
-    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request, BindingResult result){
+
+    public ResponseEntity<ErrorMessage> throwError(HttpServletRequest request, Exception ex){
+        log.error("Api Error - ", ex);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage()));
+
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(RuntimeException ex, HttpServletRequest request, BindingResult result){
         log.error("Api Error - ", ex);
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -41,5 +55,46 @@ public class ApiExceptionHandler {
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(new ErrorMessage(request, HttpStatus.NOT_FOUND,ex.getMessage()));
     }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorMessage> parseArgumentException(HttpMessageNotReadableException ex, HttpServletRequest request){
+        log.error("Api Error - ", ex);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY,"Date not in expected \"yyyy-MM-dd\" format"));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ErrorMessage> badRequestException(BadCredentialsException ex, HttpServletRequest request){
+        log.error("Api Error - ", ex);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Wrong credentials"));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorMessage> typeMismatchException(MethodArgumentTypeMismatchException ex, HttpServletRequest request){
+        log.error("Api Error - ", ex);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Argument type mismatch"));
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorMessage> wrongUrlException(NoHandlerFoundException ex, HttpServletRequest request){
+        log.error("Api Error - ", ex);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Wrong URL"));
+    }
+
+    @ExceptionHandler(TokenInvalidException.class)
+    public ResponseEntity<ErrorMessage> tokenException(TokenInvalidException ex, HttpServletRequest request){
+        log.error("Api Error - ", ex);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, ex.getMessage()));
+    }
+
 
 }
