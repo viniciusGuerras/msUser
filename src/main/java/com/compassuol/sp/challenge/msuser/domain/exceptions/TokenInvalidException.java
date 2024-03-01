d09f64a8941317ad4103be98092a40f7534d5c8a
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-03-01 02:05:52 -0300

    feat: created new exception

---
+++ src/main/java/com/compassuol/sp/challenge/msuser/domain/exceptions/TokenInvalidException.java
@@ -0,0 +1,7 @@
+package com.compassuol.sp.challenge.msuser.domain.exceptions;
+
+public class TokenInvalidException extends Exception{
+    public TokenInvalidException(String message) {
+        super(message);
+    }
+}
