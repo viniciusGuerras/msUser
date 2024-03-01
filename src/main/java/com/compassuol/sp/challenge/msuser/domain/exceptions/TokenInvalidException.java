89e4ec487945ed2d76f84473762845727917077d
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-03-01 02:06:06 -0300

    Revert "feat: created new exception"
    This reverts commit d09f64a8941317ad4103be98092a40f7534d5c8a.

--- src/main/java/com/compassuol/sp/challenge/msuser/domain/exceptions/TokenInvalidException.java
+++
@@ -1,7 +0,0 @@
-package com.compassuol.sp.challenge.msuser.domain.exceptions;
-
-public class TokenInvalidException extends Exception{
-    public TokenInvalidException(String message) {
-        super(message);
-    }
-}
