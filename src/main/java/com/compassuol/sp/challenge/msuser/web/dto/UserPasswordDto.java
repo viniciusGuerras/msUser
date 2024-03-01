28622d57b22deca13e211e167ec1978ce1321527
Vinicius Guerra <itsviniciusguerra@gmail.com>
2024-03-01 02:03:16 -0300

    feat: added @AllArgsConstructor annotation

--- src/main/java/com/compassuol/sp/challenge/msuser/web/dto/UserPasswordDto.java
+++ src/main/java/com/compassuol/sp/challenge/msuser/web/dto/UserPasswordDto.java
@@ -2,11 +2,13 @@ package com.compassuol.sp.challenge.msuser.web.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserPasswordDto {
 
     @NotBlank
